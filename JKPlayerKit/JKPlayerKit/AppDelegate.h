2c431ebfb0179d97db6d14baade7659f0e780ffa
JoanKingWC <JoanKingWC@163.com>
2019-02-11 11:03:31 +0800

    初始化项目

---
+++ JKPlayerKit/JKPlayerKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JKPlayerKit
+//
+//  Created by 王冲 on 2019/2/11.
+//  Copyright © 2019年 JK科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JKPlayerKit/JKPlayerKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JKPlayerKit
+//
+//  Created by 王冲 on 2019/2/11.
+//  Copyright © 2019年 JK科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
